db122573ac635cc30dd9690fd0fb0074c4e55d99
iwenki <139893979+iwenki@users.noreply.github.com>
2024-02-19 17:27:36 -0600

    Added footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return <div className="App"><h1>Weather App</h1>
+  <footer>This project was coded by {""}<a href="www.linkedin.com/in/vanessa-iwen-040831173" target="_blank" rel="noreferrer">K. Vanessa Iwen</a>{""} and is open-sourced on <a href="https://github.com/iwenki/react-weather-project" target="_blank" rel="noreferrer">GitHub</a></footer>
+  </div>;
 }
 
 export default App;
