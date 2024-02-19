ad162b4035f6df371a49e81cb5aa0c761851fc60
iwenki <139893979+iwenki@users.noreply.github.com>
2024-02-19 17:47:39 -0600

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
 import './App.css';
 
 function App() {
-  return <div className="App"><h1>Weather App</h1>
-  <footer>This project was coded by {""}<a href="www.linkedin.com/in/vanessa-iwen-040831173" target="_blank" rel="noreferrer">K. Vanessa Iwen</a>{""} and is open-sourced on <a href="https://github.com/iwenki/react-weather-project" target="_blank" rel="noreferrer">GitHub</a></footer>
-  </div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Weather App</h1>
+        <button type="button" className="btn btn-primary">
+          Primary
+        </button>
+        <footer>
+          This project was coded by {""}
+          <a
+            href="www.linkedin.com/in/vanessa-iwen-040831173"
+            target="_blank"
+            rel="noreferrer"
+          >
+            K. Vanessa Iwen
+          </a>
+          {""} and is open-sourced on
+          <a
+            href="https://github.com/iwenki/react-weather-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
 }
 
 export default App;
